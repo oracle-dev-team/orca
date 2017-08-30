408bd6ddaa096c1ded7724bfa55d8859c65725c7
Ryan <r.walter215@gmail.com>
2017-08-30 14:01:39 -0500

    switched out gojs for visjs

--- src/containers/InfluenceMap/InfluenceMap.js
+++ src/containers/InfluenceMap/InfluenceMap.js
@@ -1,58 +1,49 @@
 import React, {Component} from 'react';
-import go from 'gojs';
 import styles from './InfluenceMap.css'
-const $ = go.GraphObject.make;
+import vis from 'vis';
 
 export default class InfluenceMap extends Component {
   constructor (props) {
     super(props);
 
-    this.formatTree = this.formatTree.bind(this);
+
+
   }
-
-  formatTree() {
-    let myDiagram =
-      $(go.Diagram, "myDiagramDiv",
-        {
-          initialContentAlignment: go.Spot.Center, // center Diagram contents
-          "undoManager.isEnabled": true, // enable Ctrl-Z to undo and Ctrl-Y to redo
-          layout: $(go.TreeLayout, // specify a Diagram.layout that arranges trees
-                    { angle: 90, layerSpacing: 35 })
-        });
-
-    // the template we defined earlier
-    myDiagram.nodeTemplate =
-      $(go.Node, "Horizontal",
-        { background: "#44CCFF" },
-        $(go.Picture,
-          { margin: 10, width: 50, height: 50, background: "red" },
-          new go.Binding("source")),
-        $(go.TextBlock, "Default Text",
-          { margin: 12, stroke: "white", font: "bold 16px sans-serif" },
-          new go.Binding("text", "name"))
-      );
-
-    let model = $(go.TreeModel);
-    model.nodeDataArray =
-      [
-        { key: "1",              name: "Don Meow",   source: "cat1.png" },
-        { key: "2", parent: "1", name: "Demeter",    source: "cat2.png" },
-        { key: "3", parent: "1", name: "Copricat",   source: "cat3.png" },
-        { key: "4", parent: "3", name: "Jellylorum", source: "cat4.png" },
-        { key: "5", parent: "3", name: "Alonzo",     source: "cat5.png" },
-        { key: "6", parent: "2", name: "Munkustrap", source: "cat6.png" }
-      ];
-    myDiagram.model = model;
-  }
-
+  // initialize your network!
   componentDidMount() {
-    this.formatTree()
+    // create an array with nodes
+    let nodes = new vis.DataSet([
+      {id: 1, label: 'Node 1'},
+      {id: 2, label: 'Node 2'},
+      {id: 3, label: 'Node 3'},
+      {id: 4, label: 'Node 4'},
+      {id: 5, label: 'Node 5'}
+    ]);
+
+    // create an array with edges
+    let edges = new vis.DataSet([
+      {from: 1, to: 3},
+      {from: 1, to: 2},
+      {from: 2, to: 4},
+      {from: 2, to: 5}
+    ]);
+
+    // create a network
+
+    // provide the data in the vis format
+    let data = {
+      nodes: nodes,
+      edges: edges
+    };
+    let options = {};
+    let container = document.getElementById('influence');
+    let network = new vis.Network(container, data, options);
   }
+
 
   render() {
     return (
-      <div id="myDiagramDiv"style={{styles}}>
-      </div>
+      <div id="influence"></div>
     )
   }
 }
